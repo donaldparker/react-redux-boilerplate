fb870d3badd6f461d8cc41f8ef8939fe8deafa75
Donald Parker <donaldparkerii@gmail.com>
2016-08-23 16:07:25 -0700

    bootstrap sass, basic routes and component mounting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello redux');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './components/app.scss'
+
+render(
+  <Router history={ browserHistory } routes={ routes} />,
+  document.getElementById('app')
+);
--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,19 @@
 import expect from 'expect';
 
 /* es2017 async example */
-async function getResult() {
+/* const getResult = async function() {
 	var foo = await new Promise((resolve, reject) => { resolve(true); });
 	return foo;
+} */
+
+const getResults = () => {
+  return true;
 }
 
 
 describe('Just a blank test', () => {
 	it('should pass', () => {
-		let result = getResult();
+		let result = getResults();
 		console.log('Result Loaded'. result);
 		expect(result).toEqual(true);
 	});
