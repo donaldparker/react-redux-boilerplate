ba46e992c5d2bc62532d607dbe4b3959341bc94d
Donald Parker <donaldparkerii@gmail.com>
2016-08-23 19:51:25 -0700

    wire flux;

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import routes from './routes';
-import './components/app.scss'
+import './components/app.scss';
+
+/* could set initial state here for iso-render */
+const store = configureStore();
 
 render(
-  <Router history={ browserHistory } routes={ routes} />,
+  <Provider store={ store }>
+    <Router history={ browserHistory } routes={ routes} />
+  </Provider>,
   document.getElementById('app')
 );
